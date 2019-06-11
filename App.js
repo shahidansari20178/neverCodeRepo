60c408f8b4595b309876c2aa15aa29da3cdcdc4e
shahidansari20178 <45814933+shahidansari20178@users.noreply.github.com>
2019-06-11 16:37:19 +0530

    Update App.js
    changes

--- App.js
+++ App.js
@@ -24,6 +24,8 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
       <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
